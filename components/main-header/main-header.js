eeec679622310af1a7b7e7ecb4d93f49c29ec7c7
sachin barali <baralisachin5925@gmail.com>
2024-09-17 09:23:54 +0545

    header component and issued fixed

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -7,6 +7,7 @@ import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
 import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 export default function MainHeader() {
   const path = usePathname();
   return (
@@ -20,24 +21,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link
-                href="/meals"
-                className={
-                  path.startsWith("/meals") ? classes.active : undefined
-                }
-              >
-                Browse Meals
-              </Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link
-                href="/community"
-                className={
-                  path.startsWith("/community") ? classes.active : undefined
-                }
-              >
-                Foodies Community
-              </Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
